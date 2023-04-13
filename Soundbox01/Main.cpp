7c11fbc801c1558887c8febb10cc194f37a1d1ca
David <bundgaard@users.noreply.github.com>
2023-04-13 10:52:24 +0200

    Load and present the wave data

--- Soundbox01/Main.cpp
+++ Soundbox01/Main.cpp
@@ -12,6 +12,19 @@
 
 #include "Defer.h"
 #include "Win32Layer.h"
+/*
+Chart
+High Value rounded up
+Min value rounded down
+For .wav I can go with unsigned so from 0 to UINT?_MAX + nearest 0 uint16_t::max = 65 536 -> nearest thousand would be 70 000.
+
+Then we need a timeline, conversely that could be each data point or we can take the average bytes per second and read that and plot. This will give us each second as a natual time line.
+Simplicity is the key so now we could plot that data line white background and black line.
+
+Another thing to think about is how to limit the time line to our width, Excel plots large amount of numbers in intervals and smaller amounts directly.
+
+*/
+
 
 using namespace tretton63;
 
@@ -26,18 +39,77 @@ Global HWND SoundProgress;
 
 Global HFONT ButtonFont;
 
-DWORD WINAPI TickThread(LPVOID nothin)
+std::optional<WAVEDATA> Data;
+struct EventParams {
+	HANDLE Event;
+	bool shouldContinue;
+	size_t Size;
+	void* Location;
+};
+
+Global HANDLE hEvent = nullptr;
+
+[[noreturn]]
+DWORD ThreadProc(LPVOID Arguments)
 {
-	for (;;)
-	{
-		OutputDebugString(L"Tick Thread\n");
-		Sleep(1000);
-	}
+
 }
 
 Local void
 PlayAndPause_OnClick(HWND self, HWND parent)
 {
+	{
+		Local WAVEFORMATEX WaveFormatEx{};
+		HRESULT hr = S_OK;
+		auto Data = LoadWaveMMap(&WaveFormatEx, L"C:\\Code\\10562542_Liquid_Times_Original_Mix.wav");
+		if (Data.has_value())
+		{
+			if (Data->Location)
+			{
+
+				EventParams Params{};
+				Params.Event = hEvent;
+				Params.Location = Data->Location;
+				Params.Size = Data->WaveSize;
+
+				CreateThread(nullptr, 0, &ThreadProc, &Params, 0, nullptr);
+
+				if (SUCCEEDED(hr))
+				{
+					OutputDebugString(L"CreateSourceVoice\n");
+					hr = audio->CreateSourceVoice(&voice1, &WaveFormatEx, 0, XAUDIO2_DEFAULT_FREQ_RATIO);
+				}
+				puts("Try and play the note");
+
+				XAUDIO2_BUFFER XBuffer{};
+				if (Data->WaveSize <= UINT32_MAX)
+					XBuffer.AudioBytes = static_cast<uint32_t>(Data->WaveSize);
+				else
+				{
+					OutputDebugString(L"Failed to resize the WaveSize\n");
+				}
+				XBuffer.Flags = XAUDIO2_END_OF_STREAM;
+				XBuffer.pAudioData = (LPBYTE)Data->Location;
+
+				voice1->SetVolume(1.0f, XAUDIO2_COMMIT_NOW);
+
+				hr = voice1->SubmitSourceBuffer(&XBuffer);
+			}
+			Data = std::move(Data);
+		}
+
+		if (FAILED(hr))
+		{
+			DWORD dwError = GetLastError();
+			if (dwError != 0x00)
+			{
+				printf("0x%x\n", dwError);
+				printf("failed to create Voice\n");
+			}
+		}
+
+	}
+
 	std::wstring Text{};
 	auto TextLength = GetWindowTextLengthW(self);
 	Text.resize((size_t)TextLength + 1, 0);
@@ -67,7 +139,6 @@ VoiceOne_OnClick(HWND self)
 	OutputDebugString(Out.str().c_str());
 }
 
-
 LRESULT CALLBACK
 SoundboxProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
@@ -78,11 +149,12 @@ SoundboxProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 	case WM_CREATE:
 	{
 		HRESULT hr = S_OK;
+		hEvent = CreateEvent(nullptr, true, false, nullptr);
 		ButtonFont = Win32CreateFont(L"Tahoma", 14);
 
 		PauseAndPlayButton = CreateWindow(L"BUTTON", L"Play", WS_VISIBLE | WS_CHILD, 10, 10, 100, 25, hwnd, (HMENU)PauseAndPlayEvent, GetModuleHandle(nullptr), nullptr);
 		VoiceOneGetState = CreateWindow(L"BUTTON", L"Voice1 State", WS_VISIBLE | WS_CHILD, 10, 35, 100, 25, hwnd, (HMENU)VoiceOneGetStateEvent, GetModuleHandleW(nullptr), nullptr);
-		MusicFile = CreateWindow(L"EDIT", L"C:\\Code\\10847939_Ad_Finem_Original_Mix.wav", WS_VISIBLE | WS_CHILD | WS_BORDER, 10, 65, 100, 25, hwnd, (HMENU)WM_USER + 3, GetModuleHandleW(nullptr), nullptr);
+		MusicFile = CreateWindow(L"EDIT", L"C:\\Code\\10847939_Ad_Finem_Original_Mix.wav", WS_VISIBLE | WS_CHILD | WS_BORDER | ES_AUTOHSCROLL, 10, 65, 100, 25, hwnd, (HMENU)WM_USER + 3, GetModuleHandleW(nullptr), nullptr);
 
 		SendMessage(PauseAndPlayButton, WM_SETFONT, (WPARAM)(HFONT)ButtonFont, 0);
 		SendMessage(VoiceOneGetState, WM_SETFONT, (WPARAM)(HFONT)ButtonFont, 0);
@@ -98,56 +170,6 @@ SoundboxProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 			OutputDebugString(L"Create Master voice\n");
 			hr = audio->CreateMasteringVoice(&master, 2, 44100);
 		}
-
-		Local WAVEFORMATEX WaveFormatEx{};
-
-		Data = LoadWaveMMap(&WaveFormatEx, L"C:\\Code\\10847939_Ad_Finem_Original_Mix.wav");
-		if (!Data.has_value())
-		{
-			return 1;
-		}
-
-		if (SUCCEEDED(hr))
-		{
-			OutputDebugString(L"CreateSourceVoice\n");
-			hr = audio->CreateSourceVoice(&voice1, &WaveFormatEx, 0, XAUDIO2_DEFAULT_FREQ_RATIO);
-		}
-
-		if (SUCCEEDED(hr))
-		{
-
-			if (Data->Location)
-			{
-				puts("Try and play the note");
-
-				XAUDIO2_BUFFER XBuffer{};
-				if (Data->WaveSize <= UINT32_MAX)
-					XBuffer.AudioBytes = static_cast<uint32_t>(Data->WaveSize);
-				else
-				{
-					OutputDebugString(L"Failed to resize the WaveSize\n");
-				}
-				XBuffer.Flags = XAUDIO2_END_OF_STREAM;
-				XBuffer.pAudioData = (LPBYTE)Data->Location;
-
-				voice1->SetVolume(1.0f, XAUDIO2_COMMIT_NOW);
-
-				hr = voice1->SubmitSourceBuffer(&XBuffer);
-
-			}
-		}
-
-		if (FAILED(hr))
-		{
-			DWORD dwError = GetLastError();
-			if (dwError != 0x00)
-			{
-				printf("0x%x\n", dwError);
-				printf("failed to create Voice\n");
-			}
-
-		}
-
 	}
 	return 0;
 	case WM_PAINT:
@@ -179,7 +201,8 @@ SoundboxProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 	{
 		if (ButtonFont)
 			DeleteObject(ButtonFont);
-		voice1->FlushSourceBuffers();
+		if (voice1)
+			voice1->FlushSourceBuffers();
 		if (Data.has_value() && Data->Location)
 		{
 			VirtualFree(Data->Location, 0, MEM_FREE);
